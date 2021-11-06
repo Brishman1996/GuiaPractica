61ff5f369439e367f1c31dc0e962a78d2c155eb7
Brishman1996 <71751438@certus.edu.pe>
2021-11-05 22:26:44 -0500

    v3

--- TrabajoCERTUS/routes/articulo.js
+++ TrabajoCERTUS/routes/articulo.js
@@ -1,15 +1,66 @@
 const express = require('express');
-const router = express.router();
-
+const router = express.Router();
 const { nanoid } = require("nanoid");
+
+const idLength = 8;
 
 //obtener la lista de articulacion
 router.get("/", (req, res)=> {
-    const articulo = req.app.db.get("articulos").find({ id: req,params.id }).value();
+    const articulos = req.app.db.get("articulos");
 
+        res.send(articulos);       
+});
+
+//obtener un articulo desde la ID
+router.get("/:id", (req, res) => {
+    const articulo = req.app.db.get("articulos").find({ id: req.params.id }).value();
+
+    
     if(!articulo){
         res.sendStatus(404)
     }
 
-        res.send(articulo);       
+        res.send(articulo);
 });
+//crear un nuevo articulo
+router.post("/", (req, res) => {
+    try {
+        const articulo = {
+            id: nanoid(idLength),
+            ...req.body,
+        };
+
+    req.app.db.get("articulos").push(articulo).write();
+    
+    res.send(articulo)
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+//actualiza un articulo
+router.put("/:id", (req, res) => {
+    try {
+        req.app.db
+            .get("articulos")
+            .find({ id: req.params.id })
+            .assign(req.body)
+            .write();
+        
+        res.send(req.app.db.get("articulos").find({ id: req.params.id}));    
+    } catch (error)  {
+        return res.status(500).send(error);
+    }
+});
+
+//eliminar un articulo con su ID
+
+router.delete("/:id", (req, res) =>{
+    req.app.db
+    .get("articulos")
+    .remove({ id: req.params.id })
+    .write();
+
+    res.sendStatus(200);
+})
+
+module.exports = router;
